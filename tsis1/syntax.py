87eef46ffb9b179112fc1f6e7e337c1a93c06573
Otakanutyy <kogsaitama@gmail.com>
2023-01-21 17:52:38 +0500

    update

--- tsis1/syntax.py
+++ tsis1/syntax.py
@@ -4,4 +4,11 @@ if 5 > 2:
 if 5 > 2:
  print("Five is greater than two!") 
 if 5 > 2:
-        print("Five is greater than two!") 
+        print("Five is greater than two!")
+
+x = 5
+y = "Hello, World!"
+
+#This is a comment.
+print("Hello, World!")
+
