bcafaf5268a89568602416f62a8137a5ab6abee7
Otakanutyy <kogsaitama@gmail.com>
2023-03-06 18:17:11 +0600

    123

--- tsis4/math.py
+++ tsis4/math.py
@@ -1,6 +1,27 @@
+import math
+#1
+deg=int(input())
+print(math.radians(deg))
+
+#2
+print("Enter height:")
+h=int(input())
+print("Enter base1:")
+b1=int(input())
+print("Enter base2:")
+b2=int(input())
+print((b1+b2)/2*h)
+
+#3
+print("Enter num of sides:")
+num=int(input())
+print("Enter len of sides:")
+l=int(input())
+area=(num*l*l)/(4*math.tan(math.pi/num))
+print(area)
 
 #4
-base=float(input("Enter base:"))
-height2=float(input("Enter height"))
-area3=base*height2
-print(area3)
+b3=float(input("Enter base:"))
+h2=float(input("Enter height"))
+area2=b3*h2
+print(area2)
