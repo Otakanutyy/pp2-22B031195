ea7b6871f979f02fec1998394cfe2521cb6100ed
Otakanutyy <122967332+Otakanutyy@users.noreply.github.com>
2023-03-06 18:51:59 +0600

    Delete json.py

--- tsis4/json.py
+++
@@ -1,8 +0,0 @@
-import json
-import requests
-response = requests.get('https://github.com/Beisenbek/programming-principles-2/blob/main/Lab4/json/sample-data.json')
-response_json=json.loads(response.text)
-
-for var in response_json:
-    if var['mtu'] == 9150:
-        print(response_json['dn','speed','mtu'])
