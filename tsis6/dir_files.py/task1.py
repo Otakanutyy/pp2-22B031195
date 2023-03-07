41a62b957aad6ad8d8d759ecc291cb51e74fd67e
Otakanutyy <kogsaitama@gmail.com>
2023-03-07 14:03:16 +0600

    upd

--- tsis6/dir_files.py/task1.py
+++ tsis6/dir_files.py/task1.py
@@ -4,6 +4,3 @@ p=os.listdir(r"C:/Users/Ислам/Documents/week1/pp2-22B031195")
 for i in p:
     if os.path.isdir(i) or os.path.isfile(i):
         print(i)
-for i in p:
-    if os.path.isfile(i):
-        print(i)
