4ba300acc37c240cdf1f55c019dfc81788cb779a
Otakanutyy <kogsaitama@gmail.com>
2023-03-29 14:16:50 +0600

    added while pressed

--- py_game/ball.py
+++ py_game/ball.py
@@ -16,22 +16,23 @@ while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             running=False
-        if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_DOWN:
-                if(pos_y+45<=375):
-                    pos_y+=20
-            if event.key==pygame.K_UP:
-                if(pos_y-45>=-25):
-                    pos_y-=20
-            if event.key==pygame.K_RIGHT:
-                if(pos_x+45<=475):
-                    pos_x+=20
-            if event.key==pygame.K_LEFT:
-                if(pos_x-45>=-25):
-                    pos_x-=20
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        if(pos_x-45>=-40):
+             pos_x -= 5
+    if keys[pygame.K_RIGHT]:
+        if(pos_x+45<=480):
+             pos_x += 5
+    if keys[pygame.K_UP]:
+        if(pos_y-45>=-40):
+             pos_y -= 5
+    if keys[pygame.K_DOWN]:
+        if(pos_y+45<=380):
+             pos_y += 5
         
 
-    screen.fill('white')
-    pygame.draw.circle(screen, (255,204,204), (pos_x+25, pos_y+25), 25)
+    screen.fill('black')
+    pygame.draw.circle(screen, (255,255,255), (pos_x+25, pos_y+25), 25)
     pygame.display.flip()
     clock.tick(60)
